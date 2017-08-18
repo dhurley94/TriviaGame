e8b08fbd579cab7d3d4ba729b35df0768dc1fb5c
Dave Hurley <dhurley@apples-MacBook-Pro.local>
2017-08-18 02:43:17 -0400

    implemented api and init screen

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,25 @@ $(document).ready(function() {
 // https://opentdb.com/api_config.php
 // https://opentdb.com/api.php?amount=10&category=18&type=multiple
 
+
+  function generateQuestions(loop) {
+    /*
+    * pulls 'loop' amount of questions
+    * based on user input on first page
+    */
+    for (var i = 0; i < loop; i++) {
+      $.ajax({
+        url: 'https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple',
+        type: 'GET',
+        success: function(data) {
+          $.each(data, function(i, item){
+            console.log(item[0]);
+          })
+        }
+      });
+    }
+  }
+
   var game = {
     isPlaying: false,
     questionAmt: 10,
@@ -31,6 +50,8 @@ $(document).ready(function() {
   }
 
   $("#start").on('click', function() {
+    game.questionAmt = $("#initNum").val();
+    console.log(game.questionAmt);
     $(".game").show();
     $(".init").hide();
   });
