6a1d2267c189893cee29dc8f14aa9e1d3a3d1a44
Dave Hurley <dhurley@apples-MacBook-Pro.local>
2017-08-18 23:31:34 -0400

    changed

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,55 +3,51 @@ $(document).ready(function() {
 // https://opentdb.com/api_config.php
 // https://opentdb.com/api.php?amount=10&category=18&type=multiple
 
-  var triviaGame = {
-    isPlaying: false,
-    hasQuery: false,
-    questionAmt: 10,
-    questionPos: 0,
-    correct: 0,
-    wrong: 0,
-    time: 30000,
-    questionArray: []
-  }
+  var game = {
+    "isPlaying": false,
+    "hasQuery": false,
+    "questionAmt": 10,
+    "questionPos": 0,
+    "correct": 0,
+    "wrong": 0,
+    "time": 30000,
+    "questions": [],
 
-  // start game
-  function start() {
-    triviaGame.isPlaying = true;
-    triviaGame.questionAmt = $("#initNum").val();
-    triviaGame.time = triviaGame.time * triviaGame.questionAmt;
-    console.log("User selected " + triviaGame.questionAmt + " questions and has " + triviaGame.time / 1000 + " seconds to complete.");
-    $(".game").show();
-    $(".init").hide();
-    triviaGame.questionArray = getQuestions(triviaGame.questionAmt);
-    displayQuestions();
-  }
-
-  function getQuestions(loop){
+    // start game
+    start: function() {
+      game.isPlaying = true;
+      game.questionAmt = $("#initNum").val();
+      game.time = game.time * game.questionAmt;
+      console.log("User selected " + game.questionAmt + " questions and has " + game.time / 1000 + " seconds to complete.");
+      $(".game").show();
+      $(".init").hide();
+      game.getQuestions(game.questionAmt);
+      game.displayQuestion();
+    },
+    getQuestions: function(loop) {
       /*
       * pulls amount of questions
       * based on user input on first page
       * assigns it to local game variable
       */
-    $.getJSON('https://opentdb.com/api.php?amount=' + String(loop) + '&category=18&difficulty=easy&type=multiple', function(data) {
-        triviaGame.hasQuery = true;
-        return data.results;
-    });
+      $.getJSON('https://opentdb.com/api.php?amount=' + String(loop) + '&category=18&difficulty=easy&type=multiple', function(data) {
+          game.questions.push(data.results);
+      });
+    },
+    displayQuestion: function() {
+      setTimeout(function() {
+        console.log(game.questions)
+      }, 1000);
+    }
   }
 
-  function displayQuestions() {
-
-    //console.log(triviaGame.questionArray[triviaGame.questionPos])
-    //$(".query").html(triviaGame.questionArray[triviaGame.questionPos]);
-  }
-
-
-  if (triviaGame.isPlaying == false) {
+  if (game.isPlaying == false) {
     $(".game").hide();
     $(".init").show();
   }
 
   $("#start").on('click', function() {
-    start();
+    game.start();
   });
 
 });
